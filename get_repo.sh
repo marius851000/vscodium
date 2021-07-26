4099fc35e5ccc93f0afd4e2133c00b4abfac8a98
marius david <mariusdavid@laposte.net>
2021-07-26 21:23:24 +0200

    test 14e5e4272f0b49d4363b5187bc7a5284dac9bca8

--- get_repo.sh
+++ get_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMMIT=774e94e0aaee86a4490656004259de5e74358b2d
+COMMIT=14e5e4272f0b49d4363b5187bc7a5284dac9bca8
 
 git clone https://github.com/Microsoft/vscode.git --depth 1
 
@@ -12,5 +12,5 @@ popd
 # for GH actions
 if [[ $GITHUB_ENV ]]; then
 	echo "LATEST_MS_COMMIT=$COMMIT" >> $GITHUB_ENV
-	echo "LATEST_MS_TAG=git-7" >> $GITHUB_ENV
+	echo "LATEST_MS_TAG=git-8" >> $GITHUB_ENV
 fi
