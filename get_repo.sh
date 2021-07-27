184d4c7fb797feed8e09eb6dfdcc6827648546dd
marius david <mariusdavid@laposte.net>
2021-07-27 15:13:48 +0200

    fix

--- get_repo.sh
+++ get_repo.sh
@@ -5,7 +5,9 @@ export LATEST_MS_COMMIT=3edce10f204263e851d2b332b3dec2b244db7428
 export LATEST_MS_TAG=tryingagain
 echo "Got the latest MS tag: ${LATEST_MS_TAG}"
 
-git clone https://github.com/Microsoft/vscode.git --branch $LATEST_MS_TAG --depth 1
+git clone https://github.com/Microsoft/vscode.git --depth 1
+git fetch origin $LATEST_MS_COMMIT
+git checkout $LATEST_MS_COMMIT
 
 # for GH actions
 if [[ $GITHUB_ENV ]]; then
