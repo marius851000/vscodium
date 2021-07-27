0eb137fde90382e7603cff1bb5f4e406fe3c089d
marius david <mariusdavid@laposte.net>
2021-07-27 15:07:41 +0200

    continue bisection

--- get_repo.sh
+++ get_repo.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 # figure out latest tag by calling MS update API
-UPDATE_INFO=$(curl https://update.code.visualstudio.com/api/update/darwin/stable/lol)
-export LATEST_MS_COMMIT=$(echo $UPDATE_INFO | jq -r '.version')
-export LATEST_MS_TAG=$(echo $UPDATE_INFO | jq -r '.name')
+export LATEST_MS_COMMIT=3edce10f204263e851d2b332b3dec2b244db7428
+export LATEST_MS_TAG=tryingagain
 echo "Got the latest MS tag: ${LATEST_MS_TAG}"
 
 git clone https://github.com/Microsoft/vscode.git --branch $LATEST_MS_TAG --depth 1
